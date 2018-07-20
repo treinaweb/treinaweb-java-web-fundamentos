5db64944f0745599404d2bd223e06427d7482044
Cleber Campomori <cleber.campomori@gmail.com>
2018-07-20 02:59:00 -0300

    Aula 5.12: avançando no projeto do curso - criando o servlet para listagem de contatos

---
+++ tw-agenda-servlets/src/br/com/treinaweb/agenda/servlets/agenda/ListaContatosServlet.java
@@ -0,0 +1,42 @@
+package br.com.treinaweb.agenda.servlets.agenda;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.treinaweb.agenda.entidades.Contato;
+import br.com.treinaweb.agenda.repositorios.impl.ContatoRepositorioJdbc;
+import br.com.treinaweb.agenda.repositorios.interfaces.AgendaRepositorio;
+
+@WebServlet(urlPatterns = { "/agenda/listar" })
+public class ListaContatosServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3704698039659899753L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		AgendaRepositorio<Contato> agendaRepositorio = new ContatoRepositorioJdbc();
+		try {
+			List<Contato> contatos = agendaRepositorio.selecionar();
+			req.setAttribute("listaContatos", contatos);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			req.setAttribute("mensagemErro", e.getMessage());
+		}
+		RequestDispatcher dispatcher = req.getServletContext().getRequestDispatcher("/WEB-INF/paginas/agenda/listaContatos.jsp");
+		dispatcher.forward(req, resp);
+	}
+	
+	
+
+}
