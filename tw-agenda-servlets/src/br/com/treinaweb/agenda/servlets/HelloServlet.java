9d625ee2d32bf8b3c8e1392f24cea98617a2037f
Cleber Campomori <cleber.campomori@gmail.com>
2018-07-20 02:10:16 -0300

    Aula 3.4: sobrescrevendo o método service() de um servlet

--- tw-agenda-servlets/src/br/com/treinaweb/agenda/servlets/HelloServlet.java
+++ tw-agenda-servlets/src/br/com/treinaweb/agenda/servlets/HelloServlet.java
@@ -1,6 +1,7 @@
 package br.com.treinaweb.agenda.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,10 +10,22 @@ import javax.servlet.http.HttpServletResponse;
 
 public class HelloServlet extends HttpServlet {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 917942103489487808L;
+
 	@Override
-	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.service(arg0, arg1);
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Olá a partir do servlet</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<p>Olá mundo! Vim do servlet!</p>");
+		out.println("</body>");
+		out.println("</html>");
 	}
 	
 }
