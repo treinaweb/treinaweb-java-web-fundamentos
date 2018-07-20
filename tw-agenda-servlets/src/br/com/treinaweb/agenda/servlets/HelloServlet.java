61e3bd2718edc2725b7eb035e6716f3260e283a1
Cleber Campomori <cleber.campomori@gmail.com>
2018-07-20 02:08:41 -0300

    Aula 3.3: primeiros contatos com os servlets

---
+++ tw-agenda-servlets/src/br/com/treinaweb/agenda/servlets/HelloServlet.java
@@ -0,0 +1,18 @@
+package br.com.treinaweb.agenda.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.service(arg0, arg1);
+	}
+	
+}
